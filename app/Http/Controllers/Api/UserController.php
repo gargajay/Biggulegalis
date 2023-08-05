c5e26ad44df8bade8917db1c5c73196443b80ce2
Irfan <abhijnu13@gmail.com>
2023-08-05 16:24:43 +0530

    chnages get profile api

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -133,7 +133,7 @@ class UserController extends Controller
     public function getProfile(Request $request)
     {
         $userId = $request->user_id ?? Auth::id();
-        $userObject = User::where('id', )->first();
+        $userObject = User::where('id', $userId)->first();
         $userObject->makeVisible(['date_of_birth', 'biography', 'gender', 'is_profile_completed', 'push_notification', 'language']);
 
         return Helper::SuccessReturn($userObject, 'PROFILE_FETCHED');

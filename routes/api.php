00eeedfa9790ecae8ba5cee4c4e27de3f8522aa2
Ajay garg <ajay.cepoch@gmail.com>
2023-12-16 17:02:32 +0530

    added new route

--- routes/api.php
+++ routes/api.php
@@ -112,6 +112,9 @@ Route::group([], function () {
         Route::post('delete-link', [LinkController::class, 'deleteLink']);
         Route::post('get-invitation', [LinkController::class, 'getInviationList']);
         Route::post('invitation-accept-reject', [LinkController::class, 'invitationAcceptReject']);
+        Route::post('invitation_accept_reject', [LinkController::class, 'invitationAcceptReject']);
+
+        
         Route::post('send-invitation', [HomeController::class, 'sendInvitation']);
 
        

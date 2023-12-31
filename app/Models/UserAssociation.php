3c4d9dfff947ced266674e3150c03471056feb7f
Ajay garg <ajay.cepoch@gmail.com>
2023-12-31 13:57:31 +0530

    update

--- app/Models/UserAssociation.php
+++ app/Models/UserAssociation.php
@@ -45,8 +45,11 @@ class UserAssociation extends Model
         // Check if roles is present and is an array
         if (isset($roles) && is_array($roles)) {
             // Check if roles contain values between 4 and 7
-            if (collect($roles)->contains(fn ($role) => in_array($role, [4,5,6,7]))) {
+            if (collect($roles)->contains(fn ($role) => in_array($role, [4]))) {
                 $userAssociation->permissions = [1,2,3,4,5,6,7,8,9,10,11];
+            }
+            elseif(collect($roles)->contains(fn ($role) => in_array($role, [5,6,7]))){
+                $userAssociation->permissions = [1,2,3,4,6,7,8];
             }
             // Check if roles contain values 2, 3, or 8
             elseif (collect($roles)->contains(fn ($role) => in_array($role, [2, 3]))) {
